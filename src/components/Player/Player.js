b7779e0f1d7554e8f85118bbe1a18ed7208eeb5f
unknown <rashedul15-1718@diu.edu.bd>
2022-07-20 18:12:04 -0700

    Code Updated

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFootball } from '@fortawesome/free-solid-svg-icons'
 import { Col, Card, Button } from 'react-bootstrap';
 const Player = (props) => {
     const {img,name,salary} = props.player;
@@ -10,7 +12,7 @@ const Player = (props) => {
             <Card.Body>
               <Card.Title>Name: {name}</Card.Title>
               <Card.Text>Salary: {salary} million per year</Card.Text>
-              <Button onClick={()=>props.handleAddPlayer(props.player)} variant="success">Select Your Player</Button>{' '}
+              <Button onClick={()=>props.handleAddPlayer(props.player)} variant="success"><FontAwesomeIcon icon={faFootball} /> Select Your Player</Button>{' '}
             </Card.Body>
           </Card>
         </Col>
